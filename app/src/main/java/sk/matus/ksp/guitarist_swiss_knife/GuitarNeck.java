13c0333879abdd9c06f7fcc9214f988f8a7a59ba
Matúš Zeleňák <matus.zelenak.gjh@gmail.com>
2016-02-05 14:58:52 +0100

    A more sensible set of chords is generated, those generated are sorted by relevant properties afterwards. Upon clicking the tones of the chord, the tones are played.

--- app/src/main/java/sk/matus/ksp/guitarist_swiss_knife/GuitarNeck.java
+++ app/src/main/java/sk/matus/ksp/guitarist_swiss_knife/GuitarNeck.java
@@ -1,10 +1,12 @@
 package sk.matus.ksp.guitarist_swiss_knife;
 
 import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 
 /**
@@ -41,9 +43,9 @@ public class GuitarNeck {
 
         public void pick(){
             if (currentFret == -1) return;
-            System.out.println(String.format("guitar_%d_%d", stringIndex, currentFret));
             int id = context.getResources().getIdentifier(String.format("guitar_%d_%d", stringIndex, currentFret), "raw", context.getPackageName());
             player = MediaPlayer.create(context,id);
+            player.setAudioStreamType(AudioManager.STREAM_MUSIC);
             player.start();
             player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                 @Override
@@ -55,8 +57,29 @@ public class GuitarNeck {
         }
     }
 
-    class Fingering{
+    class StrumThread extends Thread{
+        ArrayList<GuitarString>strings;
+        public StrumThread(ArrayList<GuitarString>strings){
+            super();
+            this.strings = strings;
+        }
+
+        @Override
+        public void run() {
+            for (GuitarString gs : strings){
+                gs.pick();
+                try {
+                    Thread.sleep(150);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    class Fingering implements Comparable{
         ArrayList<Integer>fingering = new ArrayList<>();
+        int rating = 0;
         public Fingering(ArrayList<Integer>fingering){
             this.fingering = fingering;
         }
@@ -64,15 +87,21 @@ public class GuitarNeck {
         public String toString(){
             StringBuilder sb = new StringBuilder();
             for (Integer i : fingering){
-                if (i == -1) sb.append('x');
-                    else sb.append(i);
+                if (i == -1) sb.append('x').append(' ');
+                    else sb.append(i).append(' ');
             }
             return sb.toString();
+        }
+
+        public int compareTo(Object o){
+            Fingering f2 = (Fingering)o;
+            return f2.rating - rating;
         }
     }
 
     Context context;
     ArrayList<GuitarString>strings = new ArrayList<>();
+    int fretSpan = 3;
 
     public GuitarNeck(Context context){
         this.context = context;
@@ -91,15 +120,66 @@ public class GuitarNeck {
         if (fingering == null) return;
         for (int i = 0; i < fingering.fingering.size(); i++){
             strings.get(i).setFret(fingering.fingering.get(i));
-            strings.get(i).pick();
         }
+        StrumThread strumThread = new StrumThread(strings);
+        strumThread.run();
     }
 
 
     public ArrayList<Fingering> findFingerings(HashSet<SemiTone>chord){
         ArrayList<Fingering> result = new ArrayList<>();
-        bruteFingerings(chord,result,0,new ArrayList<Integer>(),new ArrayList<SemiTone>());
+        bruteFingerings(chord, result, 0, new ArrayList<Integer>(), new ArrayList<SemiTone>());
+        sortFingerings(result);
+        for (Fingering f : result) System.out.println(f);
         return result;
+    }
+
+    private void sortFingerings(ArrayList<Fingering>fingerings){
+        ArrayList<Fingering>result = new ArrayList<>();
+        for (Fingering f : fingerings){
+            f.rating = rateFingering(f);
+        }
+        Collections.sort(fingerings);
+    }
+
+    private int rateFingering(Fingering fingering){
+        return openStrings(fingering)*2 + continuousSeqLen(fingering)*2 - jumps(fingering)*3;
+    }
+
+    private int neckDistance(Fingering fingering){
+        return Collections.min(fingering.fingering);
+    }
+
+    private int jumps(Fingering fingering){
+        int result=0;
+        boolean b = false;
+        for (Integer i : fingering.fingering){
+            if (i == -1 && b){
+                result++;
+            } else b=true;
+        }
+        return result;
+    }
+
+    private int openStrings(Fingering fingering){
+        int result = 0;
+        for (Integer i : fingering.fingering){
+            if (i==0) result++;
+        }
+        return result;
+    }
+
+    private int continuousSeqLen(Fingering f){
+        int max = 0;
+        int gathered = 0;
+        for (int i = 0; i < f.fingering.size(); i++){
+            if (f.fingering.get(i) == -1){
+                max = Math.max(gathered,max);
+                gathered = 0;
+            } else gathered++;
+        }
+        max = Math.max(gathered,max);
+        return max;
     }
 
     private boolean isComplete(ArrayList<SemiTone>tones,HashSet<SemiTone>chord){
@@ -110,7 +190,7 @@ public class GuitarNeck {
     }
 
     private void bruteFingerings(HashSet<SemiTone> chord,ArrayList<Fingering>found, int stringIndex, ArrayList<Integer>currentFingers, ArrayList<SemiTone>currentTones){
-        if (stringIndex == 6){
+        if (stringIndex > 5){
             if (isComplete(currentTones, chord)){
                 ArrayList<Integer>newFingering = new ArrayList<>();
                 for (Integer i : currentFingers){
@@ -121,9 +201,22 @@ public class GuitarNeck {
             }
             return;
         }
-        for (int i = -1; i < 4; i++){
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (Integer i : currentFingers){
+            if (i<=0) continue;
+            min = Math.min(min,i);
+            max = Math.max(max,i);
+        }
+        int lowerBound = 1;
+        int upperBound = 15;
+        if (min != Integer.MAX_VALUE) lowerBound = Math.max(1,min-fretSpan);
+        if (max != Integer.MIN_VALUE) upperBound = Math.min(5, max + fretSpan);
+        SemiTone newTone;
+        for (int i = lowerBound; i < upperBound; i++){
+
             strings.get(stringIndex).setFret(i);
-            SemiTone newTone = strings.get(stringIndex).getSemiTone();
+            newTone = strings.get(stringIndex).getSemiTone();
             if (chord.contains(newTone)){
                 currentTones.add(newTone);
                 currentFingers.add(i);
@@ -131,11 +224,20 @@ public class GuitarNeck {
                 currentFingers.remove(currentFingers.size()-1);
                 currentTones.remove(currentTones.size()-1);
             }
-            if (newTone == null){
-                currentFingers.add(i);
-                bruteFingerings(chord,found,stringIndex+1,currentFingers,currentTones);
-                currentFingers.remove(currentFingers.size()-1);
-            }
         }
+        strings.get(stringIndex).setFret(0);
+        newTone = strings.get(stringIndex).getSemiTone();
+        boolean openString = false;
+        if (chord.contains(newTone)){
+            openString = true;
+            currentTones.add(newTone);
+            currentFingers.add(0);
+            bruteFingerings(chord,found,stringIndex+1,currentFingers,currentTones);
+            currentFingers.remove(currentFingers.size()-1);
+            currentTones.remove(currentTones.size()-1);
+        }
+        currentFingers.add(-1);
+        bruteFingerings(chord,found,stringIndex+1,currentFingers,currentTones);
+        currentFingers.remove(currentFingers.size()-1);
     }
 }
