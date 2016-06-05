de11f6b780fc1626ab86ae568557e4595041366d
Matus Zelenak <matus.zelenak.gjh@gmail.com>
2016-06-05 18:56:39 +0200

    Bugfix for metronome, added standard tempo names corresponding to bpm, moved to more reliable thread structure

--- app/src/main/java/sk/matus/ksp/guitarist_swiss_knife/MetronomeActivity.java
+++ app/src/main/java/sk/matus/ksp/guitarist_swiss_knife/MetronomeActivity.java
@@ -5,6 +5,7 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,9 +18,10 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
 
 /**
 * An activity that will act like a metronome.
@@ -27,11 +29,50 @@ import java.util.TimerTask;
 * useful now*/
 public class MetronomeActivity extends AppCompatActivity {
 
+    class Metronome extends Thread{
+
+        boolean running = false;
+
+        @Override
+        public void run() {
+            while (running){
+                if (nextBeatPosition == 0) tick("high"); else tick("low");
+                final RadioButton rb = (RadioButton)(metronomeVisualization.getChildAt(nextBeatPosition));
+                MetronomeActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (rb != null) rb.setChecked(true);
+                    }
+                });
+
+                nextBeatPosition = (nextBeatPosition + 1) % currentNoteQuantity;
+                try{
+                    Thread.sleep((int)(currentBeatDuration*1000.0));
+                }
+                catch (InterruptedException e){
+                    Log.i("WHY", "WHYYYY");
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+    class Tempo{
+        String name;
+        int lower_bound;
+        int upper_bound;
+        Tempo(String name, int upper_bound, int lower_bound){
+            this.name = name;
+            this.upper_bound = upper_bound;
+            this.lower_bound = lower_bound;
+        }
+    }
+
     private int MAXBPM = 250;
 
     private boolean isRunning = false;
     private ToggleButton toggleMetronome;
-    private Timer metronomeTimer;
     private Spinner noteCountSpinner;
     private Spinner noteFractionSpinner;
     private SeekBar tempoSeekBar;
@@ -47,22 +88,74 @@ public class MetronomeActivity extends AppCompatActivity {
     private int currentNoteQuantity = 4;
     private int nextBeatPosition = 0;
 
-    private ArrayList<RadioButton>beatVisualization;
+    private Metronome metronome = new Metronome();
 
-    class MetronomeTask extends TimerTask{
-        @Override
-        public void run(){
-            runOnUiThread(new Runnable() {
+    private ArrayList<Tempo> tempos;
 
-                @Override
-                public void run() {
-                    if (nextBeatPosition == 0) tick("high"); else tick("low");
-                    RadioButton rb = (RadioButton)(metronomeVisualization.getChildAt(nextBeatPosition));
-                    rb.setChecked(true);
-                    nextBeatPosition = (nextBeatPosition + 1) % currentNoteQuantity;
-                }
-            });
+    String lookupTempoName(int bpm){
+        for (Tempo t : tempos){
+            if (t.lower_bound <= bpm && t.upper_bound >= bpm) return t.name;
         }
+        return "";
+    }
+
+    void loadTempoNames(){
+            InputStream io = getResources().openRawResource(R.raw.tempo_pairs);
+            try {
+                tempos = readJsonStream(io);
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+    }
+
+    public ArrayList<Tempo> readJsonStream(InputStream in) throws IOException {
+        JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+        try {
+            return readTonesArray(reader);
+        }
+        finally {
+            reader.close();
+        }
+    }
+
+    /**
+     * A method to read a semitone array
+     * @param reader The JsonReader to use for reading*/
+    private ArrayList<Tempo> readTonesArray(JsonReader reader) throws IOException{
+        ArrayList<Tempo>temp = new ArrayList<>();
+        reader.beginArray();
+        while (reader.hasNext()) {
+            temp.add(readTempo(reader));
+        }
+        reader.endArray();
+        return temp;
+    }
+
+    private Tempo readTempo(JsonReader reader) throws IOException{
+        reader.beginObject();
+        String name = "";
+        int lower = 0;
+        int upper = 0;
+        while (reader.hasNext()){
+
+            String varName = reader.nextName();
+            switch (varName){
+                case "name":
+                    name = reader.nextString();
+                    break;
+                case "lower":
+                    lower = reader.nextInt();
+                    break;
+                case "upper":
+                    upper = reader.nextInt();
+                    break;
+                default: reader.skipValue();
+                    break;
+            }
+        }
+        reader.endObject();
+        return new Tempo(name, upper, lower);
     }
 
     @Override
@@ -74,16 +167,13 @@ public class MetronomeActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (isRunning){
-                    metronomeTimer.cancel();
-                    metronomeTimer.purge();
-                    isRunning = false;
+                    stopTimer();
                     nextBeatPosition = 0;
                 }
                 else
                 {
-                    metronomeTimer = new Timer();
-                    metronomeTimer.schedule(new MetronomeTask(),0,(int)(currentBeatDuration*1000.0));
-                    isRunning = true;
+                    Log.i("HERE?", "HERE");
+                    startTimer();
                 }
 
             }
@@ -97,12 +187,21 @@ public class MetronomeActivity extends AppCompatActivity {
         ArrayAdapter<Integer> spinnerArrayAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, noteCounts); //selected item will look like a spinner set from XML
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         noteCountSpinner.setAdapter(spinnerArrayAdapter);
-        noteCountSpinner.setSelection(2);
+        noteCountSpinner.post(new Runnable() {
+            @Override
+            public void run() {
+                noteCountSpinner.setSelection(3);
+            }
+        });
+        Log.i("ITEM", Integer.toString((int)noteCountSpinner.getItemAtPosition(15)));
         noteCountSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                boolean temp = isRunning;
+                stopTimer();
                 currentNoteQuantity = (int)parent.getItemAtPosition(position);
                 renderVisualisation();
+                if (temp) startTimer();
             }
 
             @Override
@@ -124,8 +223,12 @@ public class MetronomeActivity extends AppCompatActivity {
         noteFractionSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                boolean temp = isRunning;
+                stopTimer();
                 currentNoteFraction = (int)parent.getItemAtPosition(position);
                 currentBeatDuration = (4.0 / currentNoteFraction) * (60.0 / currentBpm);
+                Log.i("OR MAYBE HERE?", "MAYBE");
+                if (temp) startTimer();
             }
 
             @Override
@@ -135,11 +238,10 @@ public class MetronomeActivity extends AppCompatActivity {
         });
 
         tempoSeekBar = (SeekBar)findViewById(R.id.bpmSlider);
-        tempoSeekBar.setMax(MAXBPM);
+        tempoSeekBar.setMax(250);
         tempoSeekBar.setProgress(currentBpm);
 
         currentBpmTextView = (TextView) findViewById(R.id.currentBpmText);
-        currentBpmTextView.setText(Integer.toString(currentBpm));
 
         bpmIncreaseButton = (Button) findViewById(R.id.bmpIncreaseButton);
         bpmDecreaseButton = (Button) findViewById(R.id.bpmDecreaseButton);
@@ -171,9 +273,14 @@ public class MetronomeActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
+
+        loadTempoNames();
+
+        setBpm(currentBpm);
+
+        stopTimer();
 
         player.setOnErrorListener(new MediaPlayer.OnErrorListener() {
             @Override
@@ -197,10 +304,31 @@ public class MetronomeActivity extends AppCompatActivity {
 
     }
 
+    void stopTimer(){
+        isRunning = false;
+        if (metronome != null){
+            metronome.running = false;
+        }
+    }
+
+    void startTimer(){
+        Log.i("WUT", "?");
+        nextBeatPosition = 0;
+        if (metronome != null){
+            metronome.running = false;
+        }
+        /*metronomeTimer = new Timer();
+        metronomeTimer.schedule(new MetronomeTask(),0,(int)(currentBeatDuration*1000.0));*/
+        metronome = new Metronome();
+        metronome.running = true;
+        isRunning = true;
+        metronome.start();
+    }
+
     void setBpm(int bpm){
         if (bpm > MAXBPM || bpm <=0 ) return;
         currentBpm = bpm;
-        currentBpmTextView.setText(Integer.toString(currentBpm));
+        currentBpmTextView.setText(String.format("%d bpm (%s)", currentBpm, lookupTempoName(currentBpm)));
         tempoSeekBar.setProgress(currentBpm);
         currentBeatDuration = (4.0 / currentNoteFraction) * (60.0 / currentBpm);
     }
@@ -212,6 +340,7 @@ public class MetronomeActivity extends AppCompatActivity {
             metronomeVisualization.addView(trb);
         }
     }
+
     public boolean tick(String type){
         int id = getResources().getIdentifier(String.format("metronome_%s", type), "raw", getPackageName());
         if (id == 0) return false;
@@ -230,15 +359,42 @@ public class MetronomeActivity extends AppCompatActivity {
 
     @Override
     public void onPause() {
+        Log.i("LIFECYCLE","PAUSING");
         super.onPause();
-        metronomeTimer.cancel();
-        metronomeTimer.purge();
-        toggleMetronome = (ToggleButton) findViewById(R.id.toggleButton);
+        toggleMetronome = (ToggleButton) findViewById(R.id.metronome_toggle_button);
+        if (isRunning) {
+            stopTimer();
+            toggleMetronome.toggle();
+            isRunning = true;
+        }
     }
 
     @Override
     public void onResume(){
+        Log.i("LIFECYCLE","RESUMING");
         super.onResume();
-        metronomeTimer = new Timer();
+        if (isRunning) startTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        Log.i("LIFECYCLE","SAVINGINSTANCE");
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("NoteCount",noteCountSpinner.getSelectedItemPosition());
+        savedInstanceState.putInt("NoteFraction", noteFractionSpinner.getSelectedItemPosition());
+        savedInstanceState.putInt("nextBeat", nextBeatPosition);
+        savedInstanceState.putBoolean("Running", isRunning);
+        Log.i("RUNNING",Boolean.toString(isRunning));
+        savedInstanceState.putInt("BPM", currentBpm);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.i("LIFECYCLE","RESTORINGINSTANCE");
+        isRunning = savedInstanceState.getBoolean("Running");
+        noteCountSpinner.setSelection(savedInstanceState.getInt("NoteCount"));
+        noteFractionSpinner.setSelection(savedInstanceState.getInt("NoteFraction"));
+        nextBeatPosition = savedInstanceState.getInt("nextBeat");
+        setBpm(savedInstanceState.getInt("BPM"));
     }
 }
